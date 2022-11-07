19ca48c36ffa920e1f726da435ddc1e28e687ec1
Denis Dobra <82820448+denisdobra10@users.noreply.github.com>
2022-11-07 23:14:32 +0200

    Add files via upload

--- YoutubeDownloader/ytdownloader.py
+++ YoutubeDownloader/ytdownloader.py
@@ -1,6 +1,21 @@
-# YouTube music downloader by Denis @ 2022
-from pytube import YouTube, Playlist
+# Auto install dependencies ######################################
 import os
+import sys
+import subprocess
+
+try:
+    from pytube import YouTube, Playlist
+except:
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'pytube'])
+    from pytube import YouTube, Playlist
+
+os.system('cls' if os.name == 'nt' else 'clear')
+##################################################################
+
+
+# YouTube music downloader by Denis @ 2022
+
+
 
 pathToScript = os.path.dirname(__file__)
 pathToDownloadsFolder = pathToScript + "\Downloads"
@@ -32,7 +47,7 @@ def DownloadSong(link, path=pathToDownloadsFolder):
 # Playlist download function
 def DownloadPlaylist(link, path=pathToDownloadsFolder):
     # Setting up a counter variable to track how many songs have been successfully downloaded
-    counter = 1
+    counter = 0
 
     # Setting up the path
     downloadPath = pathToDownloadsFolder
@@ -50,7 +65,7 @@ def DownloadPlaylist(link, path=pathToDownloadsFolder):
 
             if(not "video not available" in title.lower()):
                 try:
-                    print("Download number {}: Starting downloading song: {}...".format(counter, title))
+                    print("Download number {}: Starting downloading song: {}...".format(counter + 1, title))
                     song.streams.first().download(downloadPath, fileName)
                     print("Audio has been succesfully downloaded! Name of the file: {}".format(fileName))
                     counter += 1
